8094f9b16a351511a7cfcb46d52007b51ae7da47
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2024-02-29 09:15:23 +0900

    create subaccount page

---
+++ src/app/(main)/subaccount/page.tsx
@@ -0,0 +1,38 @@
+import { redirect } from 'next/navigation';
+import React from 'react';
+
+import Unauthorized from '@/components/unauthorized';
+
+import { getAuthUserDetails, verifyAndAcceptInvitation } from '@/lib/queries';
+
+type Props = {
+  searchParams: { state: string; code: string };
+};
+
+const SubAccountMainPage = async ({ searchParams: { state, code } }: Props) => {
+  const agencyId = await verifyAndAcceptInvitation();
+
+  if (!agencyId) return <Unauthorized />;
+
+  const user = await getAuthUserDetails();
+
+  if (!user) return;
+
+  const getFirstSubaccountWithAccess = user.Permissions.find((permission) => permission.access === true);
+
+  if (state) {
+    const statePath = state.split('___')[0];
+    const stateSubaccountId = state.split('___')[1];
+    if (!stateSubaccountId) return <Unauthorized />;
+
+    return redirect(`/subaccount/${stateSubaccountId}/${statePath}?code=${code}`);
+  }
+
+  if (getFirstSubaccountWithAccess) {
+    return redirect(`/subaccount/${getFirstSubaccountWithAccess.subAccountId}`);
+  }
+
+  return <Unauthorized />;
+};
+
+export default SubAccountMainPage;
