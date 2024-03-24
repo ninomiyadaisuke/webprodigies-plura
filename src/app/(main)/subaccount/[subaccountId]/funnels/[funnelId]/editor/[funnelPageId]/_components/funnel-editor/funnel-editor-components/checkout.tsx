c8febd5e3f226f5d56ef8f971f255fdfa7f18c39
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2024-03-25 08:37:43 +0900

    create website builder

--- src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/_components/funnel-editor/funnel-editor-components/checkout.tsx
+++ src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/_components/funnel-editor/funnel-editor-components/checkout.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 'use client';
 import { EmbeddedCheckout, EmbeddedCheckoutProvider } from '@stripe/react-stripe-js';
 import clsx from 'clsx';
@@ -20,15 +21,17 @@ type Props = {
   element: EditorElement;
 };
 
-const Checkout = ({ element }: Props) => {
+const Checkout = (props: Props) => {
   const { dispatch, state, subaccountId, funnelId } = useEditor();
+
   const [clientSecret, setClientSecret] = useState('');
   const [livePrices, setLivePrices] = useState([]);
   const [subAccountConnectAccId, setSubAccountConnectAccId] = useState('');
   const options = useMemo(() => ({ clientSecret }), [clientSecret]);
-  const styles = element.styles;
+  const styles = props.element.styles;
 
   useEffect(() => {
+    if (!subaccountId) return;
     const fetchData = async () => {
       const subaccountDetails = await getSubaccountDetails(subaccountId);
       if (subaccountDetails) {
@@ -59,7 +62,6 @@ const Checkout = ({ element }: Props) => {
             prices: livePrices,
             subaccountId,
           });
-
           const response = await fetch(`${process.env.NEXT_PUBLIC_URL}api/stripe/create-checkout-session`, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
@@ -80,7 +82,6 @@ const Checkout = ({ element }: Props) => {
             className: 'z-[100000]',
             variant: 'destructive',
             title: 'Oppse!',
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
             //@ts-ignore
             description: error.message,
           });
@@ -100,7 +101,7 @@ const Checkout = ({ element }: Props) => {
     dispatch({
       type: 'CHANGE_CLICKED_ELEMENT',
       payload: {
-        elementDetails: element,
+        elementDetails: props.element,
       },
     });
   };
@@ -108,15 +109,16 @@ const Checkout = ({ element }: Props) => {
   const handleDeleteElement = () => {
     dispatch({
       type: 'DELETE_ELEMENT',
-      payload: { elementDetails: element },
+      payload: { elementDetails: props.element },
     });
   };
+
   return (
     <div
       className={clsx('relative m-[5px] flex w-full items-center justify-center p-[2px] text-[16px] transition-all', {
-        '!border-blue-500': state.editor.selectedElement.id === element.id,
+        '!border-blue-500': state.editor.selectedElement.id === props.element.id,
 
-        '!border-solid': state.editor.selectedElement.id === element.id,
+        '!border-solid': state.editor.selectedElement.id === props.element.id,
         'border-dashed border-[1px] border-slate-300': !state.editor.liveMode,
       })}
       draggable
@@ -124,11 +126,12 @@ const Checkout = ({ element }: Props) => {
       onDragStart={(e) => handleDragStart(e, 'contactForm')}
       style={styles}
     >
-      {state.editor.selectedElement.id === element.id && !state.editor.liveMode && (
+      {state.editor.selectedElement.id === props.element.id && !state.editor.liveMode && (
         <Badge className="absolute left-[-1px] top-[-23px] rounded-none rounded-t-lg ">
           {state.editor.selectedElement.name}
         </Badge>
       )}
+
       <div className="w-full border-none transition-all">
         <div className="flex w-full flex-col gap-4">
           {options.clientSecret && subAccountConnectAccId && (
@@ -138,6 +141,7 @@ const Checkout = ({ element }: Props) => {
               </EmbeddedCheckoutProvider>
             </div>
           )}
+
           {!options.clientSecret && (
             <div className="flex h-40 w-full items-center justify-center">
               <Loading />
@@ -145,7 +149,8 @@ const Checkout = ({ element }: Props) => {
           )}
         </div>
       </div>
-      {state.editor.selectedElement.id === element.id && !state.editor.liveMode && (
+
+      {state.editor.selectedElement.id === props.element.id && !state.editor.liveMode && (
         <div className="absolute right-[-1px] top-[-25px] rounded-none rounded-t-lg bg-primary  px-2.5 py-1 text-xs font-bold !text-white">
           <Trash className="cursor-pointer" onClick={handleDeleteElement} size={16} />
         </div>
